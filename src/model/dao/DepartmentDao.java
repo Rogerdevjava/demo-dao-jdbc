12b00ffc38c7d709e78e21af3ca6c61fae79d52d
Rogerio Ribeiro <rogerioribeiro@rdtechfitness.com.br>
2023-04-20 14:06:59 -0300

    SellerDaoJDBC and DaoFactoy

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -1,5 +1,15 @@
 package model.dao;
 
+import java.util.List;
+
+import model.entities.Department;
+
 public interface DepartmentDao {
 
+	void insert(Department obj);
+	void upDate(Department obj);
+	void delete(Integer Id);
+	Department findById(Integer Id);
+	List<Department>findAll();
+	
 }
--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -1,5 +1,14 @@
 package model.dao;
 
+import java.util.List;
+
+import model.entities.Seller;
+
 public interface SellerDao {
 
+	void insert(Seller obj);
+	void upDate(Seller obj);
+	void delete(Integer Id);
+	Seller findById(Integer Id);
+	List<Seller>findAll();
 }
